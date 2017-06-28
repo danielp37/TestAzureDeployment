db1162b5694022889556fca17d42d42283553c04
Dan Preece <dan.preece37@gmail.com>
2017-06-28 14:57:47 -0400

    Change to app.js

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-require("dist/index.js");
+require("http");
+
+var server = http.createServer(function(request, response) {
+
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.end("Hello Typescript on Azure!");
+
+});
+
+var port = process.env.PORT || 1337;
+server.listen(port);
+
+console.log("Server running at http://localhost:%d", port);
